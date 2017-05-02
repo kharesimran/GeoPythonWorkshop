275bba0fc6d68a03ce4ed3874bbfad57c61dad3e
Simran Khare <SIMRAN001@e.ntu.edu.sg>
2017-05-02 15:42:32 +0200

    Add files via upload

---
+++ get_utm_zone.py
@@ -0,0 +1,21 @@
+from qgis.core import *
+from qgis.gui import *
+import math
+
+@qgsfunction(args=0, group='Populated places', usesgeometry=True)
+def get_utm_zone(value1, feature, parent):
+	""" 
+	Returns the UTM zone that each feature lies in.
+	Usage: get_utm_zone()
+	"""
+	geom = feature.geometry()
+	longitude = geom.asPoint().x()
+	latitude = geom.asPoint().y()
+	zone_number = math.floor(((longitude + 180) / 6) % 60) + 1
+
+	if latitude >= 0:
+		zone_letter = 'N'
+	else:
+		zone_letter = 'S'
+
+	return '%d%s' % (zone_number, zone_letter)
