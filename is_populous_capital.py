9bc00c2d92c43b5a6ddf3107bde880f07edc37e7
Simran Khare <skhare@hsr.ch>
2017-05-07 16:39:46 +0200

    final changes

--- is_populous_capital.py
+++ is_populous_capital.py
@@ -1,10 +1,11 @@
-@qgsfunction(args='auto', group='Custom', referenced_columns=['featurecla', 'pop_max'])
-def is_populous_capital(input_pop, feature, parent):
-	""" 
-	Returns True for all country capitals with 
-	population greater than a user-defined number (input_pop).
-	Usage: is_populous_capital(200000)
-	"""
-	is_capital = feature['featurecla'] == 'Admin-0 capital'
-	is_populous = feature['pop_max'] > input_pop
-	return is_capital and is_populous
+"""
+Define new functions using @qgsfunction. feature and parent must always be the
+last args. Use args=-1 to pass a list of values as arguments
+"""
+
+from qgis.core import *
+from qgis.gui import *
+
+@qgsfunction(args='auto', group='Custom')
+def func(value1, feature, parent):
+    return value1
